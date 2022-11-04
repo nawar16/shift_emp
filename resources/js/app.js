5c93a7cd1bb1ff7f89814e5c50f2744922a53e8f
nawar16 <nawarmk98@gmail.com>
2022-11-04 07:13:06 -0700

    Handle income changes & dependencies in dist files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-import './bootstrap';
-=======
 require('./bootstrap');
->>>>>>> edeeaa1b89af8af2dc16a2bda625163283dedbd2
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import _ from 'lodash';
-window._ = _;
-=======
 window._ = require('lodash');
->>>>>>> edeeaa1b89af8af2dc16a2bda625163283dedbd2
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -11,12 +6,7 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-<<<<<<< HEAD
-import axios from 'axios';
-window.axios = axios;
-=======
 window.axios = require('axios');
->>>>>>> edeeaa1b89af8af2dc16a2bda625163283dedbd2
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -28,19 +18,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-<<<<<<< HEAD
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-=======
 // window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
@@ -48,5 +25,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
->>>>>>> edeeaa1b89af8af2dc16a2bda625163283dedbd2
 // });
